4cb024e6b441ae406c7902d9f06ceb6afb5a5239
iCodeBellevue <ca.bellevue@icodeschool.com>
2026-03-21 16:03:21 -0700

    3/21/2026

---
+++ click.js
@@ -0,0 +1,8 @@
+let number = 0
+
+
+function clicked() {
+    number = number + 1
+    document.getElementById("a-number").innerText = number
+    alert("CLICK ME")
+}
--- search.js
+++ search.js
@@ -1,22 +1,26 @@
  let itemslist = [
-    {name: "bloxycola" , price: "$250 ($1,000 per shipment)."}
+     {name: "roblox" , price: "$100000000000000 ($100000000000000000000000000 per shipment)."},
+     {name: "bloxycola" , price: "$250 ($1,000 per shipment)."},
  ]
-
 function onType() {
     
     let search = document.getElementById("search")
    
-   let subList = itemslist.filter(e =>e.name.includes(search)) 
+   let subList = itemslist.filter(e =>e.name.includes(search.value)) 
    if (search.value.length === 0) {
         subList = []
    }
-
+     //NEW
+   let resultsDiv = document.getElementById("results")
+   resultsDiv.replaceChildren()
    for (let item of subList) {
-        let scearchResult = document.createElement("div")
-        let ResultText = document.getElementById("p")
+        let searchResult = document.createElement("div")
+        let ResultText = document.createElement("p")
 
         ResultText.innerText = item.name
 
         searchResult.append(ResultText)
-   }
+        //NEW
+        resultsDiv.append(searchResult)
+   }    
 }
