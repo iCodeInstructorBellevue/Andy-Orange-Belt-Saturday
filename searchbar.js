01572b0b408cc2ca1e99620d0e9bcb08c8b02cd4
iCodeBellevue <ca.bellevue@icodeschool.com>
2026-04-04 16:05:54 -0700

    4/4/2026

--- searchbar.js
+++ searchbar.js
@@ -1,21 +1,63 @@
 let itemsList = [
-    {name: "air", price: "10000000000"},
-    {name: "table", price: "29"},
-    {name: "beans", price: "89"},
+    {name: "air", price: "-10000000000", img: "https://www.cookmuseum.org/wp-content/uploads/2020/09/air-image-cropped.jpg"},
+    {name: "table", price: "29", img: "https://www.simpli-home.com/cdn/shop/files/AXCDNT-003-OAK_Silo_Detail_1.jpg?v=1765468260&width=2560"},
+    {name: "beans", price: "89", img: "https://www.recipetineats.com/tachyon/2014/05/Homemade-Heinz-Baked-Beans_0-SQ.jpg?resize=500%2C500"},
 ]
 
 function onType() {
     let searchbar = document.getElementById("searchbar")
+    let subList = itemsList.filter(
+        e => e.name.includes(searchbar.value)
+    )
 
-    let subList = itemsList
-            .filter(e => e.name.includes(searchbar.value))
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     if (searchbar.value.length === 0) {
         subList = []
     }
 
-    // NEW
+
+
+
+
+
     let resultsDiv = document.getElementById("results")
     resultsDiv.replaceChildren()
+
+
+
+
+
 
     for (let item of subList) {
         let searchResult = document.createElement("div")
@@ -23,9 +65,45 @@ function onType() {
 
         resultText.innerText = item.name
 
-        searchResult.append(resultText)
+        resultText.addEventListener("click", onResultClick.bind(null, item.price, item.img))
 
-        // NEW
+        searchResult.append(resultText)
         resultsDiv.append(searchResult)
     }
+}
+
+function onResultClick(priceIn, imgLinkIn, eventIn) {
+    let cartDiv = document.getElementById("cart-section")
+
+    let cartText = document.createElement("p")
+    cartText.innerText = eventIn.target.innerText + " :: " + priceIn
+
+    let cartImg = document.createElement("img")
+    cartImg.src = imgLinkIn
+
+    cartDiv.append(cartImg)
+    cartDiv.append(cartText)
+
+
+
+
+
+
+
+    cartText.addEventListener("click", (eventIn_) => {
+        let popup = document.createElement("div")
+        popup.className = "popup"
+        let popupText = document.createElement("p")
+        popupText.innerText = "what?"
+        popup.append(popupText)
+        document.body.append(popup)
+
+        let cartTextRect = cartText.getBoundingClientRect()
+        popup.style.left = cartTextRect.left + window.scrollX
+        popup.style.top = cartTextRect.top - 15 - cartTextRect.height + window.scrollY
+        
+        setTimeout(() => {
+            popup.remove()
+        }, 1500)
+    })
 }
