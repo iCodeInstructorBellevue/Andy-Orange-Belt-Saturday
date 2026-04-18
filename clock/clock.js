2a85b1d76718a19f713f2e336acaac9c58a1b5c7
iCodeBellevue <ca.bellevue@icodeschool.com>
2026-04-18 16:12:25 -0700

    4/18/2026

--- clock/clock.js
+++ clock/clock.js
@@ -1,3 +1,22 @@
 function updateTime(){
-    let timeElement = document.
+    let timeElement = document.getElementById("time")
+ 
+    let date =  new Date()
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+    let milliseconds = date.getMilliseconds()
+if (milliseconds <=999 && milliseconds >=100){
+milliseconds = "0" + milliseconds
 }
+if (seconds<10){
+    seconds = "0"+ seconds
+}
+if (minutes<10){
+    minutes = "0"+ minutes
+}
+if (hours<10){
+    hours = "0"+ hours
+}
+    timeElement.innerText = hours + ":" + minutes + ":" + seconds + ":" + milliseconds
+}   setInterval(updateTime)
