7b69482ecbec50a292dba59e2c37eac939e9fe0f
iCodeBellevue <ca.bellevue@icodeschool.com>
2026-05-02 16:06:39 -0700

    5/2/2026

---
+++ grayBeltS4Review.py
@@ -0,0 +1,11 @@
+
+his_name = "5" #string type
+age = 5 #int type
+exact_age = 5.0 #float type
+
+my_classroom = ["Mr. Gio", "Mahreen", "Arshaam"]
+classroom_ages =[8,9,19]
+funny_memes = ["ai baby", 67, "big chungus", 6.7]
+
+print(funny_memes[3])
+
