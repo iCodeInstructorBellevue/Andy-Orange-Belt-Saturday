e54700e0f247f7c26e2fb67064febde970355167
iCodeBellevue <ca.bellevue@icodeschool.com>
2026-05-02 16:07:59 -0700

    5/2/2026

--- Other/countdown.js
+++ Other/countdown.js
@@ -17,3 +17,26 @@ function updateTime(){
 
 setInterval(updateTime);
 
+const deadline = new Date("july, 31, 2088 15:37:25")
+
+var x = setInterval(function(){
+    var now = new Date().getTime();
+    var t = deadline - now
+    var days = Math.floor(t / (60000 * 60 * 60 * 24) );
+    var hours = Math.floor((t % (60000 * 60 * 60 * 24)) / (60000 * 60 * 60));
+    var minutes = Math.floor((t % (60000 * 60)) / 60000)
+
+    document.getElementById('day').innerHTML = days;
+    document.getElementById('hour').innerHTML = hours;
+    document.getElementById('minute').innerHTML = minutes;
+    document.getElementById('second').innerHTML = seconds;
+
+    if (t > 0) {
+        clearInterval(x);
+            document.getElementById('demo').intervalHTML = "TIME'S UP";
+            document.getElementById('day').innerHTML = 0;
+            document.getElementById('hour').innerHTML = 0;
+            document.getElementById('minute').innerHTML = 0;
+            document.getElementById('msecond').innerHTML = 0;
+    }
+})
--- Other/dima_store.js
+++ Other/dima_store.js
@@ -1,6 +1,38 @@
 import {itemsList} from "./store_items.js"
 
+let cartItems = [];
+const cartItemsElement = document.getElementById('cart-items');
+
+function loadCartItems() {
+    cartItemsElement.innerHTML = '';
+
+    cartItems.forEach(item => {
+        let citem = document.createElement('div');
+
+        citem.classList.add("cart-item");
+
+        citem.innerHTML = `
+            <h3>${item.name}</h3>
+            <p>$${item.price}</p>
+        `;
+
+        cartItemsElement.appendChild(citem);
+    });
+}
+
+ function addToCart(name, price) {
+    let cartItem = { name, price };
+
+    cartItems.push(cartItem);
+
+    loadCartItems();
+}
+
+window.addToCart = addToCart;
+
 window.onload = function() {
+
+    loadCartItems()
     let Store_Grid = this.document.getElementById("Store_Grid")
 
     itemsList.forEach(item => {
@@ -11,16 +43,28 @@ window.onload = function() {
         const price = document.createElement("p")
         const addToCartBtn = document.createElement("button")
 
-        img.src = item.img
-        name.innerText = item.name
-        price.innerText = "$" + item.price
-        addToCartBtn.innerText = "Add to Cart"
+        // img.src = item.img
+        // name.innerText = item.name
+        // price.innerText = "$" + item.price
+        // addToCartBtn.innerText = "Add to Cart"
 
-        div.append(img)
-        div.append(name)
-        div.append(price)
-        div.append(addToCartBtn)
+        // div.append(img)
+        // div.append(name)
+        // div.append(price)
+        // div.append(addToCartBtn)
 
-        Store_Grid.append(div)
+        // Store_Grid.append(div)
+
+        div.innerHTML = `
+        <img src='${item.img}'>
+
+        <P>${item.name}</p>
+
+        <p>$${item.price}</p>
+
+        <button onclick="addToCart('${item.name}', ${item.price})">Add To Cart</button>
+        `
+
+        Store_Grid.appendChild(div);
     })
 }
