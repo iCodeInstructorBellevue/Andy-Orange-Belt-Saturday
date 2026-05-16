23d2e7792328724d244017b4507a0ab87369b68c
iCodeBellevue <ca.bellevue@icodeschool.com>
2026-05-16 16:06:02 -0700

    5/16/2026

--- doodlejunp/script.js
+++ doodlejunp/script.js
@@ -1,5 +1,5 @@
 const gameWidth = 400;
-const gameWidth = 600;
+const gameHeight = 600;
 let player = {
     x:175,
     y:450,
@@ -18,3 +18,58 @@ let camera = 0;
 let moveLeft=false;
 let moveRight=false;
 
+const playerElement = document.getElementById('player')
+const scoreElement = document.getElementById('score')
+const gameOverElement = document.getElementById('gameOver')
+const finalScoreElement = document.getElementById('finalScore')
+const startScreenElements = document.getElementById('startScreen')
+const gameContainer = document.getElementById('gameContainer')
+
+function createPlatform(x, y) {
+ const isBreakble = Math.random() < 0.2; 
+  const isBouncy = Math.random() < 0.15; 
+ const platform = {
+    x: x,
+    y: y,
+    width: 70*Math.random(),
+    height: 15,
+    element: null,
+    isBreakble: isBreakble,
+    isBouncy: isBouncy,
+    isBroken: false
+ };
+    const platformElement = document.createElement('div');
+    platformElement.className = 'platform';
+    if (platform.isBreakble) {
+        platformElement.classList.add('breaking-platform');
+    }
+      if (platform.isBouncy) {
+        platformElement.classList.add('bouncy-platform');
+    }
+    platformElement.style.left = platform.x + 'px';
+    platformElement.style.top = platform.y + 'px';
+      platformElement.style.width = platform.width + 'px';
+    gameContainer.appendChild(platformElement);
+
+    platform.element = platformElement;
+    return platform;
+}
+
+
+
+
+function initalizePlatform() {
+    platforms = [];
+
+
+    for (let i = 0; i < 7; i++) {
+        const x = Math.random() * (gameWidth - 70);
+        const y = i * 90 + 50;
+        platforms.push(createPlatform(x, y));
+    }
+
+    platforms.push(createPlatform(player.x - 10, player.y + player.height));
+}
+   playerElement.style.top = player.y + 'px';
+   playerElement.style.left = player.x + 'px';
+initalizePlatform();
