181cba96e75134b08878ba0b99ec3a60fd85f662
iCodeBellevue <ca.bellevue@icodeschool.com>
2026-05-30 16:11:09 -0700

    5/16/2026

---
+++ New folder/script.js
@@ -0,0 +1,21 @@
+const gameWidth = 400;
+const gameHeight = 600;
+
+let player = {
+    x: 175,
+    y: 450,
+    width: 50,
+    height: 50,
+    velocityY: 0,
+    jumpPower: -15,
+};
+
+let score = 0;
+let gameRunning = false;
+let platforms = [];
+let camera = 0;
+
+let moveLeft = false;
+let moveRight = false;
+
+const playerElement = document.getElementById('player');
