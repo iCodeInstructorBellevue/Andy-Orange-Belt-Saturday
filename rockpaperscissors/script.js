6ea17b7d63ad4ce020377800ff25c16b5932e026
iCodeBellevue <ca.bellevue@icodeschool.com>
2026-08-22 16:05:55 -0700

    8/22/2026

--- rockpaperscissors/script.js
+++ rockpaperscissors/script.js
@@ -4,7 +4,7 @@ const playerHandEl = document.getElementById('player-hand');
 const cpuHandEl = document.getElementById('cpu-hand');
 const playerHandImg = playerHandEl.querySelector('img');
 const cpuHandImg = cpuHandEl.querySelector('img');
-const result = document.querySelector('.result p');
+const resultText = document.querySelector('.result p');
 const optionButtons = document.querySelectorAll('.options .choice-btn');
 const gameContainer = document.querySelector('.game-container');
 
@@ -15,7 +15,7 @@ let cpuScore = 0;
 const choices = ['rock', 'paper', 'scissors'];
 
 
-optionsButtons.forEach(button => {
+optionButtons.forEach(button => {
     button.addEventListener('click', () => {
         const playerChoice = button.dataset.choice;
         playRound(playerChoice);
@@ -24,6 +24,23 @@ optionsButtons.forEach(button => {
 
 function playRound(playerChoice){
 toggleButtons(true);
+
+resultText.textContent = "Rock, Paper, Scissors Shoot!";
+
+
+playerHandImg.src = 'images/logo.png';
+cpuHandImg.src = 'images/logo.png';
+
+setTimeout(() => {
+    playerHandEl.classList.remove('shake');
+    cpuHandEl.classList.remove('shake');
+    const cpuChoice = choices[Math.floor(Math.random() * choices.length)];
+    playerHandImg.src = `images/${playerChoice}.png`;
+    cpuHandImg.src = `images/${cpuChoice}.png`;
+}, 1600)
+
+playerHandEl.classList.add('shake');
+cpuHandEl.classList.add('shake');
 }
 
 function determineWinner(player, cpu){
