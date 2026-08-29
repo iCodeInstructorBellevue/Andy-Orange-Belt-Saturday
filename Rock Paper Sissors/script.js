6e1d39032a732633ea224ba7243e39a3715ba85b
iCodeBellevue <ca.bellevue@icodeschool.com>
2026-08-29 16:05:21 -0700

    8/29/2026

--- Rock Paper Sissors/script.js
+++ Rock Paper Sissors/script.js
@@ -1,9 +1,10 @@
 const playerScoreE1 = document.getElementById('player-score');
-const computerScoreE1 = document.getElementById('bot-score');
+const botScoreEl = document.getElementById('bot-score');
 const playerHandEl = document.getElementById('player-hand');
 const botHandEl = document.getElementById('bot-hand');
 const playerHandImg = botHandEl.querySelector('img');
-const computerHandImg = computerHandEl.querySelector('.result p');
+const botHandImg = botHandEl.querySelector('img');
+const resultText = document.querySelector('.result p')
 const optionButtons = document.querySelectorAll('.options .choice-btn');
 const gameContainer = document.querySelector('.game-container');
 
@@ -19,15 +20,63 @@ optionButtons.forEach(button => {
 })
 
 function playRound(playerChoice){
+    toggleButtons(true);
 
+    resultText.textContent = "Rock, Paper, Scissors";
+
+    playerHandEl.classList.add('shake');
+    botHandEl.classList.add('shake');
+
+    setTimeout(() => {
+        playerHandEl.classList.remove('shake');
+        botHandEl.classList.remove('shake');
+
+        const botChoice = choices[Math.floor(Math.random() * choices.length)];
+
+            playerHandImg.src = `images/${playerChoice}.png`;
+            botHandImg.src = `images/${botChoice}.png`;
+
+            const winner = determineWinner(playerChoice, botChoice);
+
+            updateScoreboard(winner);
+
+            toggleButtons(false);
+    }, 1600)
 }
 
 function determineWinner(player, bot){
-
+    if (player === bot) {
+        return 'tie';
+    }
+    if (
+        (player === 'rock' && bot === 'scissors') ||
+        (player === 'paper' && bot === 'rock') ||
+        (player === 'scissors' && bot === 'paper')
+    ) {
+        return 'player';
+    }
+    return 'bot';
 }
 
 function updateScoreboard(winner){
+    gameContainer.classList.remove('player-wins-transform');
 
+    if (winner === 'player') {
+        resultText.textContent = "You Win! 🎉";
+        playerScore++;
+        playerScoreEl.textContent = playerScore;
+
+        playerScoreEl.classList.add('score-updated');
+        gameContainer.classList.add('player-wins-transform')
+    } else if (winner === 'bot') {
+        resultText.textContent = "You Lose! 😢";
+        botScore++;
+        botScoreEl.textContent = botScore;
+
+        botScoreEl.classList.add('score-updated');
+    } else{
+        resultText.textContent = "It's a tie! 🤝";
+    }
 }
 
 function toggleButtons(disabled){
